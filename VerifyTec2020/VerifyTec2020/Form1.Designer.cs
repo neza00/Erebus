833ea36146946d43b9cdf51ec3bdf47f1944aa68
Ray <reiky311@hotmail.com>
2020-07-27 16:47:04 +0800

    Add Mulit Thread Test

--- VerifyTec2020/VerifyTec2020/Form1.Designer.cs
+++ VerifyTec2020/VerifyTec2020/Form1.Designer.cs
@@ -29,15 +29,28 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(22, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -67,17 +80,115 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(103, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(670, 166);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(22, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Mulit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Parameters:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Thread Num";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(170, 198);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(63, 21);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(250, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Target";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(297, 198);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(151, 21);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Delegate Verify";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -87,6 +198,13 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
     }
 }
 
--- VerifyTec2020/VerifyTec2020/Form1.cs
+++ VerifyTec2020/VerifyTec2020/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace VerifyTec2020
 {
@@ -20,6 +21,9 @@ namespace VerifyTec2020
         public Form1()
         {
             InitializeComponent();
+            numericUpDown1.Value = 4;
+            numericUpDown2.Value = 10000000;
+            chart1.Series.Clear();
             timer1.Interval = 100;
             timer1.Start();
             sysTimer = new System.Timers.Timer();
@@ -37,6 +41,20 @@ namespace VerifyTec2020
                 string msg = string.Format("[{0}] Enable Counter.\n",
                     DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
                 LogMsgs.Add(msg);
+            }
+            if (MulitRunning == true)
+            {
+                bool tempret = true;
+                for (int i = 0; i < ThreadNum; i++)
+                {
+                    tempret &= resultState[i];
+                }
+                if (tempret == true)
+                {
+                    NeedUpdateChart = true;
+                    MulitRunning = false;
+                }
+                
             }
         }
 
@@ -111,6 +129,31 @@ namespace VerifyTec2020
                     richTextBox1.ScrollToCaret();
                     LogMsgs.RemoveAt(0);
                 }
+
+                if(NeedUpdateChart == true)
+                {   chart1.Series.Clear();
+                    if (chart1.Titles.Count == 0)
+                    {
+                        chart1.Titles.Add("Mulit Threads Test");
+                    }
+                    Series s = new Series($"{ThreadNum} Threads");
+                    s.ChartType = SeriesChartType.Bar;
+                    TotalTime = 0;
+                    for (int i = 0; i< ThreadNum; i++)
+                    {
+                        s.Points.AddXY($"T{i}", Convert.ToDouble(Result[i]));
+                        TotalTime += Result[i];
+                    }
+                    chart1.Series.Add(s);
+                    string msg = string.Format("Total Time: {0} ms\n", TotalTime);
+                    LogMsgs.Add(msg);
+                    NeedUpdateChart = false;
+                    numericUpDown1.Enabled = true;
+                    numericUpDown2.Enabled = true;
+                    button3.Enabled = true;
+                    button3.Focus();
+                }
+
             }
             catch (Exception ex)
             {
@@ -123,6 +166,66 @@ namespace VerifyTec2020
         private void button2_Click(object sender, EventArgs e)
         {
             richTextBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            NeedUpdateChart = false;
+            resultState = new List<bool>();
+            Result = new Dictionary<int, long>();
+            long target = ThreadTarget;
+            for(int i = 0; i< ThreadNum; i++)
+            {
+                resultState.Add(false);
+                Result.Add(i, 0);
+                Thread thread = new Thread(MulitPlusCounter);
+                List<object> para = new List<object>();
+                para.Add(i);
+                para.Add(target);
+                thread.Start(para);
+            }
+            numericUpDown1.Enabled = false;
+            numericUpDown2.Enabled = false;
+            MulitRunning = true;
+        }
+
+        bool MulitRunning = false;
+        List<bool> resultState = new List<bool>();
+        Dictionary<int, long> Result = new Dictionary<int, long>();
+        readonly object thislock = new object();
+        bool NeedUpdateChart = false;
+        int ThreadNum = 0;
+        long TotalTime = 0;
+        long ThreadTarget = 0;
+
+        private void MulitPlusCounter(object para)
+        {
+            List<object> paras = para as List<object>;
+            int index = Convert.ToInt32(paras[0]);
+            long target = Convert.ToInt64(paras[1]);
+            Stopwatch sw = new Stopwatch();
+            string msg = string.Format("[{0}] Thread<{1}> started.\n",
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), index);
+            LogMsgs.Add(msg);
+            sw.Start();
+            for (long i = 0; i < target; i++) ;
+            sw.Stop();
+            Result[index] = sw.ElapsedMilliseconds;
+            msg = string.Format("[{0}] Thread<{1}> stopped.\n",
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), index);
+            LogMsgs.Add(msg);
+            resultState[index] = true;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            ThreadTarget = Convert.ToInt64(numericUpDown2.Value);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            ThreadNum = Convert.ToInt32(numericUpDown1.Value);
         }
     }
 }
