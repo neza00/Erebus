852a594fc8737ab5079c197b30414393c1e15b11
Ray <reiky311@hotmail.com>
2020-07-27 11:42:12 +0800

    Add single process test

--- VerifyTec2020/VerifyTec2020/Form1.Designer.cs
+++ VerifyTec2020/VerifyTec2020/Form1.Designer.cs
@@ -29,12 +29,64 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Single";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 229);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(751, 209);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(698, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Delegate Verify";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- VerifyTec2020/VerifyTec2020/Form1.cs
+++ VerifyTec2020/VerifyTec2020/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,115 @@ namespace VerifyTec2020
 {
     public partial class Form1 : Form
     {
+
+        System.Timers.Timer sysTimer;
+
         public Form1()
         {
             InitializeComponent();
+            timer1.Interval = 100;
+            timer1.Start();
+            sysTimer = new System.Timers.Timer();
+            sysTimer.Interval = 100;
+            sysTimer.Elapsed += SysTimer_Elapsed;
+            sysTimer.Start();
+        }
+
+        private void SysTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (counterEnable == false && counterNeedStart == true)
+            {
+                counterEnable = true;
+                counterNeedStart = false;
+                string msg = string.Format("[{0}] Enable Counter.\n",
+                    DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                LogMsgs.Add(msg);
+            }
+        }
+
+        bool counterNeedStart = false;
+        bool counterEnable = false;
+
+        private delegate void PlusCounterEndCallback(object target);
+
+        private PlusCounterEndCallback PCC;
+        private void PlusCounter(object para)
+        {
+            while (counterEnable == false)
+            {
+                Thread.Sleep(10);
+            }
+
+            string msg = string.Format("[{0}] Counter Start to work with target of {1}.\n",
+                DateTime.Now.ToString("yyyy-MM-ss HH:mm:ss.fff"), para);
+            LogMsgs.Add(msg);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int target = Convert.ToInt32(para);
+            for (int i = 0; i < target; i++)
+            {
+
+            }
+            sw.Stop();
+            counterEnable = false;
+            msg = string.Format("[{0}] Counter Fin the work by {1}ms.\n",
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), sw.ElapsedMilliseconds);
+            LogMsgs.Add(msg);
+            PCC?.Invoke(para);
+
+        }
+
+        private void PCCEndProcess(object para)
+        {
+            string msg = string.Format("[{0}] This is end process.\n",
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+            LogMsgs.Add(msg);
+        }
+       
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg = string.Format("[{0}] Pluscounter Started.\n",
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+            LogMsgs.Add(msg);
+
+            PCC = new PlusCounterEndCallback(PCCEndProcess);
+            Thread thread = new Thread(PlusCounter);
+            thread.Start(100000000);
+            counterNeedStart = true;
+
+            msg = string.Format("[{0}] Pluscounter Stopped.\n",
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+            LogMsgs.Add(msg);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (LogMsgs.Count > 0)
+                {
+                    if (richTextBox1.SelectionColor != Color.Blue)
+                    {
+                        richTextBox1.SelectionColor = Color.Blue;
+                    }
+                    richTextBox1.AppendText(LogMsgs[0]);
+                    richTextBox1.ScrollToCaret();
+                    LogMsgs.RemoveAt(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        List<string> LogMsgs = new List<string>();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
         }
     }
 }
